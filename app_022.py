7218cde29824f87506d33283d3ac6acf8187a4a3
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-17 21:40:09 +0200

    0022: app_022 QListWidget demo 2

---
+++ app_022.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QCheckBox, QComboBox, QListWidget, QLineEdit,
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
+)
+from PyQt5.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = QListWidget()
+        widget.addItems(["One", "Two", "Three"])
+
+        widget.currentItemChanged.connect(self.index_changed)
+        widget.currentTextChanged.connect(self.text_changed)
+
+        self.setCentralWidget(widget)
+
+
+    def index_changed(self, i): # i — не индекс, а сам QList
+        print(i.text())
+
+    def text_changed(self, s): # s — это строка
+        print(s)
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
