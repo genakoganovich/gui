7ea55d7b86165b1d583aeea1233eb82f968e482b
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-17 21:28:49 +0200

    0020: app_020 QCheckBox demo 2

---
+++ app_020.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QCheckBox, QComboBox, QListWidget, QLineEdit,
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
+)
+from PyQt5.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = QCheckBox()
+        widget.setCheckState(Qt.CheckState.Checked)
+
+        # Включение трёх состояний: widget.setCheckState(Qt.PartiallyChecked)
+        # Или: widget.setTriState(True)
+        widget.stateChanged.connect(self.show_state)
+
+        self.setCentralWidget(widget)
+
+    def show_state(self, s):
+        print(s == Qt.CheckState.Checked)
+        print(s)
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
