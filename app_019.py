f21e989c97000d16b411d7cd198e841043fba026
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-17 20:16:41 +0200

    0019: app_019 QLabel demo 2

---
+++ app_019.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QCheckBox, QComboBox, QListWidget, QLineEdit,
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
+)
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = QLabel("Hello")
+        font = widget.font()
+        font.setPointSize(30)
+        widget.setFont(font)
+        widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+
+        self.setCentralWidget(widget)
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
