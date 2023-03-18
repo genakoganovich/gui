f4231cefbd168e66af58f40d00d9902f65d3b7db
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-18 13:12:39 +0200

    0033: app_033 Nested layouts with margins and spaces

--- part2/app_033.py
+++ part2/app_033.py
@@ -25,7 +25,7 @@ class MainWindow(QMainWindow):
         layout2 = QVBoxLayout()
         layout3 = QVBoxLayout()
 
-        layout1.setContentsMargins(0, 0, 0, 0)
+        layout1.setContentsMargins(0,0,0,0)
         layout1.setSpacing(20)
 
         layout2.addWidget(Color('red'))
