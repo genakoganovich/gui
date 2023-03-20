24929edb0115c0b315b0a5b2593c3fe1b7b89e58
gennady <genakoganovich@gmail.com>
2023-03-20 16:34:26 +0200

    0049: app_049 CustomDialog with ok, cancel buttons

--- part2/app_047.py
+++ part2/app_047.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog
 
 
 class MainWindow(QMainWindow):
